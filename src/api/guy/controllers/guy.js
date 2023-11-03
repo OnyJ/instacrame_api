8d4e7a4c34f6944bb1bee3a7aa7b3c3bb26c76f9
Josselin <julien.josselin75@hotmail.fr>
2023-11-03 14:56:33 +0100

    init api ; with models User, Post, Guy

---
+++ src/api/guy/controllers/guy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * guy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::guy.guy');
---
+++ src/api/guy/routes/guy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * guy router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::guy.guy');
